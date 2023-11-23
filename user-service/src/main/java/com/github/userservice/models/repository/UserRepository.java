af957af9f98a343237a6c38eb563cd218ab3886b
robertperquim <perquimrobert@gmail.com>
2023-11-22 22:03:36 -0300

    feat: Adicionando User Repository

---
+++ user-service/src/main/java/com/github/userservice/models/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.github.userservice.models.repository;
+
+import com.github.userservice.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
