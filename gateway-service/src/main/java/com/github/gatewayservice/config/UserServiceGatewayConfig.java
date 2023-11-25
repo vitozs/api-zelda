db07fd40cbcb389d41813a4ffcc03bf012f3be0c
robertperquim <perquimrobert@gmail.com>
2023-11-25 02:08:27 -0300

    Feat: Adicionando config de rotas dos endpoints da user-service

---
+++ gateway-service/src/main/java/com/github/gatewayservice/config/UserServiceGatewayConfig.java
@@ -0,0 +1,31 @@
+package com.github.gatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserServiceGatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("user-service-userList", r -> r.path("/user/usersList")
+                        .uri("http://localhost:8085/"))
+                .route("user-service-update", r -> r.path("/user/update")
+                        .uri("http://localhost:8085/"))
+                .route("user-service-profile", r -> r.path("/user/profile/**")
+                    .filters(f -> f.rewritePath("/(?<id>.*)", "/${id}"))
+                    .uri("http://localhost:8085/"))
+                .route("user-service-create", r -> r.path("/user/create")
+                        .uri("http://localhost:8085/"))
+                .route("user-service-delete", r -> r.path("/user/delete/**")
+                        .filters(f -> f.rewritePath("/(?<id>.*)", "/${id}"))
+                        .uri("http://localhost:8085/"))
+                .build();
+    }
+
+
+
+}
