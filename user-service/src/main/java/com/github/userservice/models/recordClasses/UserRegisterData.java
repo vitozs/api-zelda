8e22f1a17e6e813323d33a98a9ac42e81da770f5
robertperquim <perquimrobert@gmail.com>
2023-11-26 02:50:57 -0300

    Fix: Adicioando email e password nos dtos

--- user-service/src/main/java/com/github/userservice/models/recordClasses/UserRegisterData.java
+++ user-service/src/main/java/com/github/userservice/models/recordClasses/UserRegisterData.java
@@ -1,9 +1,6 @@
 package com.github.userservice.models.recordClasses;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public record UserRegisterData(
         @NotBlank
@@ -11,6 +8,11 @@ public record UserRegisterData(
         @NotNull
         @Min(12)
         @Max(80)
-        Long age
+        Long age,
+        @Email
+        String email,
+        @NotBlank
+        String password
 ) {
+
 }
