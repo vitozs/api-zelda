0850c4ae6e19aa2bfe5c8a946c61553cf8337d6f
robertperquim <perquimrobert@gmail.com>
2023-11-26 02:51:50 -0300

    feat: Adicioandno metodo findByemail no userrepository

--- user-service/src/main/java/com/github/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/github/userservice/repository/UserRepository.java
@@ -2,8 +2,10 @@ package com.github.userservice.repository;
 
 import com.github.userservice.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel,Long> {
+    UserDetails findByemail(String username);
 }
