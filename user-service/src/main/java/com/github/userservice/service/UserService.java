e58e03ab69fe4806a0a2706b3f074606cbe1d63a
robertperquim <perquimrobert@gmail.com>
2023-12-01 20:24:14 -0300

    Fix: package

--- user-service/src/main/java/com/github/userservice/service/UserService.java
+++ user-service/src/main/java/com/github/userservice/service/UserService.java
@@ -9,7 +9,7 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,7 +18,7 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
     public UserDetalingData creatUser(UserRegisterData data){
         UserModel userModel = new UserModel(data);
