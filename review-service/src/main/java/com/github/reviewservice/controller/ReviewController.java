7509b600aefcb3128e9bac177332826a2dd80e0a
Vitor Chagas <vitornuneschagas2016@gmail.com>
2023-12-04 20:53:04 -0300

    feat: service para as reviews

---
+++ review-service/src/main/java/com/github/reviewservice/controller/ReviewController.java
@@ -0,0 +1,35 @@
+package com.github.reviewservice.controller;
+
+import com.github.reviewservice.reviewsDTO.ReviewResponseDTO;
+import com.github.reviewservice.service.UserReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("review")
+public class ReviewController {
+
+    @Autowired
+    UserReviewService userReviewService;
+
+    @GetMapping("get-all")
+    public ResponseEntity<List<ReviewResponseDTO>> getAllReviews(@RequestHeader HttpHeaders headers){
+        return new ResponseEntity<>(userReviewService.getAllReviews(headers), HttpStatus.OK);
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<?> createReview(@RequestHeader HttpHeaders headers, @RequestBody ReviewRequestDTO analiseUser){
+        return new ResponseEntity<>(userReviewService.createReview(headers, analiseUser), HttpStatus.OK);
+    }
+
+    @PostMapping("update")
+    public ResponseEntity<?> createReview(@RequestHeader HttpHeaders headers, @RequestParam String id, @RequestBody ReviewRequestDTO analiseUser){
+        return new ResponseEntity<>(userReviewService.updateReview(headers, id, analiseUser), HttpStatus.OK);
+    }
+
+}
---
+++ review-service/src/main/java/com/github/reviewservice/service/UserReviewService.java
@@ -0,0 +1,66 @@
+package com.github.reviewservice.service;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.github.reviewservice.repository.ReviewRepository;
+
+import com.github.reviewservice.reviewsDTO.ReviewModel;
+import com.github.reviewservice.reviewsDTO.ReviewRequestDTO;
+import com.github.reviewservice.reviewsDTO.ReviewResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserReviewService {
+
+    @Autowired
+    ReviewRepository reviewRepository;
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    private String decodeJWT(HttpHeaders headers){
+        String token = headers.getFirst(HttpHeaders.AUTHORIZATION); //pega o token
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer("UserToken")
+                .build();
+
+        DecodedJWT decodedJWT = verifier.verify(token.replaceAll("Bearer ", "")); // formata e valida o token
+
+        String email =  decodedJWT.getSubject();
+        return email;
+    }
+
+    public List<ReviewResponseDTO> getAllReviews(HttpHeaders headers){
+        String email = decodeJWT(headers);
+        return reviewRepository.getAllReviewsByUser(email);
+    }
+
+
+    public ReviewModel createReview(HttpHeaders headers, ReviewRequestDTO analiseUser) {
+        String email = decodeJWT(headers);
+        ReviewModel analise = new ReviewModel();
+        analise.setId_usuario(reviewRepository.getIdByEmail(email));
+        analise.setId_jogo(reviewRepository.getGameIdByCode(analiseUser.getId_jogo()));
+        analise.setAnalise(analiseUser.getAnalise());
+
+        return reviewRepository.save(analise);
+
+
+    }
+
+    public ReviewModel updateReview(HttpHeaders headers, String id, ReviewRequestDTO analiseUser){
+        String email = decodeJWT(headers);
+        ReviewModel analise = reviewRepository.getReferenceById(Long.valueOf(id));
+        analise.setAnalise(analiseUser.getAnalise());
+        return reviewRepository.saveAndFlush(analise);
+
+    }
+}
