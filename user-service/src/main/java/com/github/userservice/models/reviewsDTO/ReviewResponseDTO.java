5ebe0c0db0695052d663fca6a5ad2fb535036017
Vitor Chagas <vitornuneschagas2016@gmail.com>
2023-12-04 19:15:24 -0300

    feat: DTO da query customizada das analises

---
+++ user-service/src/main/java/com/github/userservice/models/reviewsDTO/ReviewResponseDTO.java
@@ -0,0 +1,9 @@
+package com.github.userservice.models.reviewsDTO;
+
+public interface ReviewResponseDTO {
+
+    public String getId();
+    public String getUsername();
+    public String getGame();
+    public String getAnalise();
+}
