366f7d206f85691513873c94365fd5e8a558c7b7
robertperquim <perquimrobert@gmail.com>
2023-12-07 06:09:30 -0300

    Fix: Arrumando atributos para padrao das tabelas

--- zelda-service/src/main/java/com/github/zeldaservice/model/favoriteModel/FavoriteGameModel.java
+++ zelda-service/src/main/java/com/github/zeldaservice/model/favoriteModel/FavoriteGameModel.java
@@ -4,12 +4,12 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Table(name = "user_and_favorite_game")
+@Table(name = "usuario_e_jogo_favorito")
 @Data
 public class FavoriteGameModel {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long id_user;
-    private Long id_game;
+    private Long id_usuario;
+    private Long id_jogo;
 }
