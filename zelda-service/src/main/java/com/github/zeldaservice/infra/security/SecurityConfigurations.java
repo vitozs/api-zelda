05457ff139410fb6784ddfb72f45b37c259fda83
Vitor Chagas <vitornuneschagas2016@gmail.com>
2023-12-07 02:28:41 -0300

    feat:route liberation

--- zelda-service/src/main/java/com/github/zeldaservice/infra/security/SecurityConfigurations.java
+++ zelda-service/src/main/java/com/github/zeldaservice/infra/security/SecurityConfigurations.java
@@ -20,6 +20,7 @@ public class SecurityConfigurations {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll();
                     req.anyRequest().authenticated();
                 })
                 .addFilterBefore(securityZeldaFilter, UsernamePasswordAuthenticationFilter.class)
