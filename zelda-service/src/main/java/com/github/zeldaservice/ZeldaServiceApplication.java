2af4b75af8f92e2d4c93d792c852c9e9fe7cf259
Vitor Chagas <vitornuneschagas2016@gmail.com>
2023-12-07 02:28:11 -0300

    feat:swagger annotation on main class

--- zelda-service/src/main/java/com/github/zeldaservice/ZeldaServiceApplication.java
+++ zelda-service/src/main/java/com/github/zeldaservice/ZeldaServiceApplication.java
@@ -1,9 +1,14 @@
 package com.github.zeldaservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Api zelda", version = "1", description = "Api retorna dados dos principais jogos do zelda" ))
+@EnableScheduling
 public class ZeldaServiceApplication {
 
 	public static void main(String[] args) {
