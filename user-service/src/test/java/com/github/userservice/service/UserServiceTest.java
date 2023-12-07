e02267e182d5f1e7279774f7fa5731a14bd010c7
robertperquim <perquimrobert@gmail.com>
2023-12-07 01:35:40 -0300

    fix: Refatorando testes unitarios

--- user-service/src/test/java/com/github/userservice/service/UserServiceTest.java
+++ user-service/src/test/java/com/github/userservice/service/UserServiceTest.java
@@ -1,11 +1,12 @@
 package com.github.userservice.service;
 
+import com.github.userservice.infra.security.SecurityFilter;
 import com.github.userservice.models.UserModel;
 import com.github.userservice.models.recordClasses.UserDetalingData;
 import com.github.userservice.models.recordClasses.UserRegisterData;
 import com.github.userservice.models.recordClasses.UserUpdateData;
 import com.github.userservice.repository.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +14,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.mockito.ArgumentMatchers.any;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -24,28 +28,38 @@ class UserServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private SecurityFilter securityFilter;
+
+    @Mock
+    private TokenService tokenService;
+
     @Test
     void createUser_deveSalvarOUser() {
         // Arrange
-        UserRegisterData userRegisterData = new UserRegisterData("Jonas", 19L);
-        UserModel userModel = new UserModel(1L, "Jonas", 19L);
-
-        Mockito.when(userRepository.save(Mockito.any())).thenReturn(userModel);
+        UserRegisterData userRegisterData = new UserRegisterData("Jonas", 19L, "jonas@gmail.com", "123456");
 
         // Act
         userService.creatUser(userRegisterData);
 
         // Assert
-        Mockito.verify(userRepository).save(Mockito.any());
+        Mockito.verify(userRepository).save(any());
     }
 
     @Test
     void createUser_deveRetornarOUserComUmId() {
         // Arrange
-        UserRegisterData userRegisterData = new UserRegisterData("Jonas", 19L);
-        UserModel userModel = new UserModel(1L, "Jonas", 19L);
+        UserRegisterData userRegisterData = new UserRegisterData("Jonas", 19L, "jonas@gmail.com", "123456");
+        UserModel userModel = new UserModel(userRegisterData);
 
-        Mockito.when(userRepository.save(Mockito.any())).thenReturn(userModel);
+        Mockito.when(userRepository.save(any(UserModel.class))).thenAnswer(invocation -> {
+            UserModel savedUserModel = invocation.getArgument(0);
+            savedUserModel.setId(1L);
+            return savedUserModel;
+        });
 
         // Act
         UserDetalingData resultado = userService.creatUser(userRegisterData);
@@ -54,31 +68,24 @@ class UserServiceTest {
         Assertions.assertThat(resultado.id()).isEqualTo(1L);
     }
 
-    @Test
-    void updateUser_deveLancarExcecaoQuandoUserNaoExiste() {
-        // Arrange
-        Long userId = 1L;
-        UserUpdateData userUpdateData = new UserUpdateData(userId, "NovoNome", 25L);
-
-        Mockito.when(userRepository.getReferenceById(userId))
-                .thenThrow(EntityNotFoundException.class);
-
-        // Act & Assert
-        Assertions.assertThatThrownBy(() -> userService.updateUser(userUpdateData))
-                .isInstanceOf(EntityNotFoundException.class);
-    }
 
     @Test
-    void updateUser_deveAlterarOsDadosDoUserQuandoEleExiste() {
+    void updateUser_deveAlterarOsDadosDoUser() {
         // Arrange
         Long userId = 1L;
-        UserUpdateData userUpdateData = new UserUpdateData(userId, "NovoNome", 25L);
-        UserModel existingUser = new UserModel(userId, "NomeAntigo", 20L);
+        UserUpdateData userUpdateData = new UserUpdateData("NovoNome", 25L, "NovoEmail@gmail.com");
+        UserModel existingUser = new UserModel(userId, "NomeAntigo", 20L, "emailAntigo@gmail.com", "123456");
+
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+
+
+        Mockito.when(userService.recoverId(any())).thenReturn(userId);
+
 
         Mockito.when(userRepository.getReferenceById(userId)).thenReturn(existingUser);
 
         // Act
-        UserDetalingData resultUserDetails = userService.updateUser(userUpdateData);
+        UserDetalingData resultUserDetails = userService.updateUser(userUpdateData, request);
 
         // Assert
         Assertions.assertThat(resultUserDetails.id()).isEqualTo(userId);
@@ -90,57 +97,40 @@ class UserServiceTest {
     void getProfileUser_deveRetornarUserComIdSolicitado() {
         // arrange
         Long userId = 1L;
-        UserModel userModel = new UserModel();
+        UserModel userModel = new UserModel(userId, "Jonas", 19L, "jonas@gmail.com", "123456");
+
         UserDetalingData expectedUserDetails = new UserDetalingData(userModel);
 
         // mock do userRepository
         Mockito.when(userRepository.getReferenceById(userId)).thenReturn(userModel);
 
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+
+        Mockito.when(userService.recoverId(any())).thenReturn(userId);
         //act
-        UserDetalingData resultUserDetails = userService.getProfileUser(userId);
+        UserDetalingData resultUserDetails = userService.getProfileUser(request);
 
         // assert
         Assertions.assertThat(resultUserDetails).isEqualTo(expectedUserDetails);
     }
 
+
+
     @Test
-    void getProfileUser_deveLancarExcecaoQuandoUserNaoExiste() {
+    void deleteUser_deveDeletarOUser() {
         // Arrange
         Long userId = 1L;
 
-        Mockito.when(userRepository.getReferenceById(userId))
-                .thenThrow(EntityNotFoundException.class);
+        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
 
-
-        // Act & Assert
-        Assertions.assertThatThrownBy(() -> userService.getProfileUser(userId))
-                .isInstanceOf(EntityNotFoundException.class);
-    }
-
-    @Test
-    void deleteUser_deveDeletarOUserQuandoEleExiste() {
-        // Arrange
-        Long userId = 1L;
-
-        Mockito.when(userRepository.existsById(userId)).thenReturn(true);
+        Mockito.when(userService.recoverId(any())).thenReturn(userId);
 
         // Act
-        userService.deleteUser(userId);
+        userService.deleteUser(request);
 
         // Assert
         Mockito.verify(userRepository).deleteById(userId);
     }
 
-    @Test
-    void deleteUser_deveLancarExcecaoQuandoEleNaoExiste() {
-        // Arrange
-        Long userId = 1L;
 
-        Mockito.when(userRepository.existsById(userId)).thenReturn(false);
-
-
-        // Act & Assert
-        Assertions.assertThatThrownBy(() -> userService.deleteUser(userId))
-                .isInstanceOf(EntityNotFoundException.class);
-    }
 }
