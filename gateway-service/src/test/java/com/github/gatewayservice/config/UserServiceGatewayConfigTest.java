e5105ac7aea6f7e622e0ad5a05d72bb4d689abfc
robertperquim <perquimrobert@gmail.com>
2023-12-07 03:09:52 -0300

    Feat: Adiconando teste para rota de login com atenticacao valida

---
+++ gateway-service/src/test/java/com/github/gatewayservice/config/UserServiceGatewayConfigTest.java
@@ -0,0 +1,33 @@
+package com.github.gatewayservice.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+public class UserServiceGatewayConfigTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    public void login_DeveRetornar200() {
+        webTestClient.post().uri("/login/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("{ \n" +
+                        "    \"email\" : \"robert@gmail.com\",\n" +
+                        "    \"password\": \"12345678\"\n" +
+                        "}")
+                .exchange()
+                .expectStatus().isOk();
+
+    }
+
+
+    }
