4e83e039f313d09859fbc943f54bb6ef88bbab2b
Vitor Chagas <vitornuneschagas2016@gmail.com>
2023-12-23 15:56:23 -0300

    fix: database config

--- resources/init.sql
+++ resources/init.sql
@@ -1,7 +1,66 @@
 create table users(
-	id serial primary key,
-	name varchar(50) not null,
-	age int not null
+    id serial primary key,
+    name varchar(100) not null,
+    age integer not null,
+    email  varchar(100) unique not null,
+    password  varchar(255) not null
 );
 
-insert into users(name, age) values('Artur', 33), ('Lorenzo', 21);
+CREATE TABLE jogos(
+	id serial primary key,
+   name          VARCHAR(255)
+  ,description   text
+  ,developer     VARCHAR(50)
+  ,publisher     VARCHAR(50)
+  ,released_date DATE
+  ,id_jogo            VARCHAR(255)
+);
+
+create table usuario_e_jogo_favorito(
+	id serial primary key,
+	id_usuario int not null,
+	id_jogo int not null,
+	foreign key (id_usuario) references users(id),
+	foreign key (id_jogo) references jogos(id)
+);
+
+create table analises(
+	id serial primary key,
+	id_usuario int not null,
+	id_jogo int not null,
+	analise text,
+	foreign key (id_usuario) references users(id),
+	foreign key (id_jogo) references jogos(id)
+
+);
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda','The Legend of Zelda is the first installment of the Zelda series. It centers its plot around a boy named Link, who becomes the central protagonist throughout the series. It came out as early as 1986 for the Famicom in Japan, and was later released in the western world, including Europe and the US in 1987. It has since then been re-released several times, for the Nintendo GameCube as well as the Game Boy Advance. The Japanese version of the game on the Famicom is known as The Hyrule Fantasy: The Legend of Zelda. ','Nintendo R&D 4','Nintendo',' February 21, 1986','5f6ce9d805615a85623ec2b7');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: A Link to the Past','One day, a band of evil thieves managed to open the gateway to the Sacred Realm, where the mystical Triforce was hidden. Upon finding the sacred golden relic, the leader of the thieves, Ganondorf, slew his followers and claimed it as his own. Before long, dark power began to flow forth from the Sacred Realm. People were drawn into this darkness, and never heard from again. As a result, the King of Hyrule ordered the seven sages to seal the entrance to the Sacred Realm. A great battle ensued—monsters poured into the Light World from the sacred land and attacked the castle. The Knights of Hyrule defended the sages during the great battle against evil, and, though most of them perished in the struggle, the sages were able to cast their seal, stopping the flow of darkness and trapping the evil king Ganon within. This battle became known as the Imprisoning War. ','Nintendo','Nintendo',' April 13, 1992','5f6ce9d805615a85623ec2b8');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Ocarina of Time','The Legend of Zelda: Ocarina of Time is the fifth main installment of The Legend of Zelda series and the first to be released for the Nintendo 64. It was one of the most highly anticipated games of its age. It is listed among the greatest video games ever created by numerous websites and magazines. Released in the United States on November 23, 1998, it was the first game in The Legend of Zelda series that was visually displayed in 3D . ','Nintendo EAD','Nintendo',' November 23, 1998','5f6ce9d805615a85623ec2ba');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Oracle of Ages','The Legend of Zelda: Oracle of Ages is one of two The Legend of Zelda  titles released for the Game Boy Color, the other being Oracle of Seasons, both representing the seventh and eighth main installments of the series. Released near the end of the system''s lifespan, Oracle of Ages and its counterpart were said to "send the Game Boy Color out with a bang." In anticipation of the upcoming release of the Game Boy Color''s successor, the Game Boy Advance, the games exhibited special features  when played on the new handheld system. ','Capcom','Nintendo',' May 14, 2001','5f6ce9d805615a85623ec2b9');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Link''s Awakening DX','The game was also made available on the Nintendo 3DS eShop on June 7, 2011 at the price of $5.99 US. ','Nintendo','Nintendo',' December 15, 1998','5f6ce9d805615a85623ec2bb');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Majora''s Mask','The Legend of Zelda: Majora''s Mask is the sixth main installment of The Legend of Zelda series, first released on the Nintendo 64 in 2000. Unique among The Legend of Zelda series, the game includes a time system that spans three days, and this cycle must be reset periodically to progress through the game. Majora''s Mask is one of the few Zelda games in which Ganon does not play any role whatsoever. In addition, Princess Zelda does not play a major role; she is only seen once in a flashback scene from Ocarina of Time. ','Nintendo EAD','Nintendo',' October 26, 2000','5f6ce9d805615a85623ec2bc');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('Zelda II: The Adventure of Link','A few years after the defeat of Ganon and the rescue of Princess Zelda, Link, now at the age of sixteen, is disturbed by the appearance of a mark on the back of his hand. Upon seeing this mark, Impa, the nurse of Princess Zelda, tells him the story of how, ages ago, the King of Hyrule had hidden a third part of the Triforce, the Triforce of Courage, in the Great Palace to safeguard it from evil. ','Nintendo EAD','Nintendo',' January 14, 1987','5f6ce9d805615a85623ec2bd');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: The Wind Waker','The Legend of Zelda: The Wind Waker is the tenth main installment of The Legend of Zelda series. It is the first Zelda game for the Nintendo GameCube, and was released in Japan on December 13, 2002, in Canada and the United States on March 24, 2003, in South Korea on April 16, 2003, in Europe on May 2, 2003 and in Australia on May 7, 2003. ','Nintendo EAD','Nintendo',' March 24, 2003','5f6ce9d805615a85623ec2bf');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Twilight Princess','The Legend of Zelda: Twilight Princess is the thirteenth main installment of The Legend of Zelda series, released for both the Nintendo GameCube and Wii. It was highly anticipated by many members of the gaming community and was regarded as finally fulfilling the dreams of those who wanted a much more realistic and mature Zelda game, as seen in the SpaceWorld 2000 GameCube Tech Demo. This is the first Zelda game to be rated T by ESRB and 12+ by PEGI. The reason is probably because of violence, blood , and signs of nudity . This game is also notable for being the first console Zelda title released in the United States before Japan, as the Wii version was released in America on November 19, 2006, whereas the Japanese versions were released on December 2. Because of this, Twilight Princess was one of the launch titles for the Wii alongside Wii Sports in the United States. ','Nintendo EAD','Nintendo',' November 19, 2006','5f6ce9d805615a85623ec2be');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Oracle of Seasons','The Legend of Zelda: Oracle of Seasons is one of two The Legend of Zelda  titles released for the Game Boy Color, the other being Oracle of Ages, both representing the seventh and eighth main installments of the series. Released near the end of the system''s lifespan, Oracle of Seasons and its counterpart were said to "send the Game Boy Color out with a bang." In anticipation of the upcoming release of the Game Boy Color''s successor, the Game Boy Advance, the games exhibited special features  when played on the new handheld system. ','Capcom','Nintendo',' May 14, 2001','5f6ce9d805615a85623ec2c0');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Spirit Tracks',' ','Nintendo EAD','Nintendo',' December 7, 2009','5f6ce9d805615a85623ec2c5');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('BS The Legend of Zelda: Ancient Stone Tablets','BS The Legend of Zelda: Ancient Stone Tablets is a downloadable four-part episodic game that was available for the BS-X Broadcasting System add-on for the Super Famicom , and the third title in the BS Zelda series. Each episode was downloaded from St. GIGA''s satellite radio service and stored on memory packs but could only be played once a week during the broadcast period. ','Nintendo','St. GIGA','March 30, 1997','5f6ce9d805615a85623ec2ca');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('Hyrule Warriors','Hyrule Warriors is a Legend of Zelda spin-off game for the Wii U that was released on the 19th of September 2014, the 20th of September 2014, the 26th of September 2014 and the 14th of August 2014 in Europe, Australia, North America and Japan respectively. It combines the world of The Legend of Zelda  with the action of Koei Tecmo''s Dynasty Warriors series and was developed by Team Ninja and Omega Force. ','Omega Force','Nintendo',' September 26, 2014','5f6ce9d805615a85623ec2cf');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Four Swords Adventures','One night, an ominous cloud covers Hyrule, throwing fear into the people. Princess Zelda calls upon her most trusted childhood friend, Link. She wants to check up on the Four Sword, fearing that the seal that imprisoned the evil wind sorcerer Vaati inside of it at the end of Four Swords might have weakened. Inside the castle they meet up with the gathered six Maidens, whose purpose is to protect Hyrule as well as the Four Sword Sanctuary. With the help of the maidens Zelda summons a portal to the sanctuary.  ','Nintendo','Nintendo',' June 7, 2004','5f6ce9d805615a85623ec2c1');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Tri Force Heroes','The Legend of Zelda: Tri Force Heroes is the eighteenth main installment of The Legend of Zelda series. The game was released on October 23, 2015 in North America and Europe, October 24 in Australia, and October 22 in Japan. It was revealed during E3 2015 on June 16. ','Nintendo','Nintendo',' October 23, 2015','5f6ce9d805615a85623ec2d4');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Breath of the Wild','The Legend of Zelda: Breath of the Wild is the nineteenth main installment of The Legend of Zelda series. It was released simultaneously worldwide for the Wii U and Nintendo Switch on March 3, 2017','Nintendo','Nintendo',' March 3, 2017','5f6ce9d805615a85623ec2c9');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Tri Force Heroes','The Legend of Zelda: Tri Force Heroes is the eighteenth main installment of The Legend of Zelda series. The game was released on October 23, 2015 in North America and Europe, October 24 in Australia, and October 22 in Japan. It was revealed during E3 2015 on June 16. ','Nintendo','Nintendo',' October 23, 2015','5f6ce9d805615a85623ec2ce');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('Zelda: The Wand of Gamelon','A product of a compromise between Nintendo and Philips due to their failure to release a CD-ROM based add-on to the Super Nintendo Entertainment System, The Wand of Gamelon, alongside the other two, are the only licensed The Legend of Zelda  games developed for and released on a non-Nintendo system. The games have been subject to much criticism, and Nintendo does not recognize them as canon to The Legend of Zelda series. ','Animation Magic','Philips Media',' October 10, 1993','5f6ce9d805615a85623ec2d3');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: Four Swords','As part of the 25th Anniversary of The Legend of Zelda series, an enhanced port of the game was released for the Nintendo DSi and Nintendo 3DS as a limited-time free download, Four Swords Anniversary Edition. ','Capcom','Nintendo',' December 2, 2002','5f6ce9d805615a85623ec2c4');
+INSERT INTO jogos(name,description,developer,publisher,released_date,id_jogo) VALUES ('The Legend of Zelda: The Minish Cap','The Legend of Zelda: The Minish Cap is the twelfth main installment of The Legend of Zelda series. It was released for the Game Boy Advance in 2004. ','Capcom','Nintendo',' January 10, 2005','5f6ce9d805615a85623ec2c6');
+
+insert into users(name, age, email, password) values ('Vitor', 18, 'vitornuneschagas2016@gmail.com', '123456');
+insert into usuario_e_jogo_favorito(id_usuario, id_jogo) values (1, 2);
+insert into usuario_e_jogo_favorito(id_usuario, id_jogo) values (1, 4);
+insert into analises(id_usuario, id_jogo, analise) values (1, 2, 'Muito ruim slk kkkkkkkkkkkkk');
+insert into analises(id_usuario, id_jogo, analise) values (1, 4, 'Muito bommmm slk kkkkkkkkkkkkk');
+
+
+insert into users(name, age, email, password) values ('Leo', 28, 'leonardo@gmail.com', '123456');
+insert into usuario_e_jogo_favorito(id_usuario, id_jogo) values (2, 4);
+insert into analises(id_usuario, id_jogo, analise) values (2, 2, 'Muito bommm adoreiii');
